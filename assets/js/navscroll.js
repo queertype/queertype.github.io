405521bef1e4e54e2b0e7498f13b9ad087e455ab
betsyschuster <will@queerfonts.com>
2024-08-06 20:40:16 +0200

    trying to fix this

--- assets/js/navscroll.js
+++ assets/js/navscroll.js
@@ -2,6 +2,7 @@ $(window).scroll(function () {
     document.title = window.scrollY
     var scrollTop = window.scrollY;
 
+if ($(window).width() < 600) {
     if (scrollTop > 10) {
             $(".nav").css({
                 "filter": "drop-shadow(0px 0px 10px #3C50FF)",
@@ -14,13 +15,14 @@ $(window).scroll(function () {
                 "width": "3.5em"
             });
         }
-    ;
+    };
+if ($(window).width() < 600) {
 
     if (scrollTop == 0) {
 
             $(".nav").css({
 
-                "filter": "drop-shadow(0px 0px 100px #3C50FF)",
+                "filter": "drop-shadow(0px 0px 0px #FFF6FE)",
                 "padding-top": ".8em",
                 "padding-bottom": ".4em"
 
@@ -31,6 +33,7 @@ $(window).scroll(function () {
         }
 
     }
+}
 
 );
 
@@ -69,3 +72,5 @@ $(window).on("resize", function () {
 
     ;
 });
+
+
