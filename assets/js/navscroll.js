377840cd2db2190dfbc9c61a4954c5f7ac085fea
betsyschuster <will@queerfonts.com>
2024-08-06 21:32:38 +0200

    Update navscroll.js

--- assets/js/navscroll.js
+++ assets/js/navscroll.js
@@ -2,35 +2,35 @@ $(window).scroll(function () {
     document.title = window.scrollY
     var scrollTop = window.scrollY;
 
-if ($(window).width() < 600) {
-    if (scrollTop > 10) {
+    if ($(window).width() < 600) {
+        if (scrollTop > 10) {
             $(".nav").css({
                 "filter": "drop-shadow(0px 0px 10px #3C50FF)",
                 "padding-top": ".4em",
                 "padding-bottom": ".2em"
 
-            });
+            })
 
             $(".logo").css({
                 "width": "3.5em"
-            });
+            })
         } else {
             $(".nav").css({
                 "filter": "drop-shadow(0px 0px 0px #3C50FF)",
                 "padding-top": ".8em",
                 "padding-bottom": ".4em"
 
-            });
+            })
 
             $(".logo").css({
                 "width": "3.5em"
-            });
+            })
         }
 
     };
 }
 
-);
+)
 
 
 
@@ -47,17 +47,17 @@ $(window).on("resize", function () {
 
             $(".logo").css({
                 "width": "3.5em"
-            });
+            })
         } else if (w > 600) {
             $(".nav").css({
                 "filter": "drop-shadow(0px 0px 0px #3C50FF)",
                 "padding-top": ".8em",
                 "padding-bottom": ".4em"
 
-            });
+            })
             $(".logo").css({
                 "width": "3.5em"
-            });
+            })
 
         }
 
@@ -67,10 +67,10 @@ $(window).on("resize", function () {
             "padding-top": "1em",
             "padding-bottom": ".4em"
 
-        });
+        })
 
         $(".logo").css({
             "width": "5em"
-        });
+        })
     }
 })
