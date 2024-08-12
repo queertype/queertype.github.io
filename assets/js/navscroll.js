f8dbfbd5d7c7bcfffac66f17a50c689f703814ca
betsyschuster <will@queerfonts.com>
2024-08-12 03:49:51 +0200

    Update navscroll.js
    Courtesy of miister gpt

--- assets/js/navscroll.js
+++ assets/js/navscroll.js
@@ -1,78 +1,40 @@
-$(window).scroll(function () {
-    document.title = window.scrollY
-    var scrollTop = window.scrollY;
+$(window).on("scroll resize", function () {
+    // Get the current scroll position and window width
+    var scrollTop = $(window).scrollTop();
+    var windowWidth = $(window).width();
 
-    if ($(window).width() < 600) {
+    // Determine if the width is less than 600px
+    var isMobile = windowWidth < 600;
+
+    // Apply styles based on scroll position and window width
+    if (isMobile) {
         if (scrollTop > 10) {
             $(".nav").css({
                 "filter": "drop-shadow(0px 0px 10px #3C50FF)",
                 "padding-top": ".4em",
                 "padding-bottom": ".2em"
-
-            })
-
+            });
             $(".logo").css({
                 "width": "3.5em"
-            })
+            });
         } else {
             $(".nav").css({
                 "filter": "drop-shadow(0px 0px 0px #FFF6FE)",
                 "padding-top": ".8em",
                 "padding-bottom": ".4em"
-
             });
-
             $(".logo").css({
                 "width": "4.5em"
-            })
-        }
-
-    };
-}
-
-)
-
-
-
-$(window).on("resize", function () {
-    document.title = window.scrollY
-    var scrollTop = window.scrollY;
-    var w = $(this).width();
-    if ($(window).width() < 600) {
-        if (scrollTop < 10) {
-            $(".nav").css({
-                "filter": "drop-shadow(0px 0px 0px #FFF6FE)",
-                "padding-top": ".8em",
-                "padding-bottom": ".4em"
-
             });
-
-            $(".logo").css({
-                "width": "3.5em"
-            })
-        } else if (w > 600) {
-            $(".nav").css({
-                "filter": "drop-shadow(0px 0px 0px #FFF6FE)",
-                "padding-top": ".8em",
-                "padding-bottom": ".4em"
-
-            });
-            $(".logo").css({
-                "width": "3.5em"
-            })
-
         }
-
     } else {
         $(".nav").css({
             "filter": "drop-shadow(0px 0px 0px #FFF6FE)",
             "padding-top": "1em",
             "padding-bottom": ".4em"
-
         });
-
         $(".logo").css({
             "width": "5em"
-        })
+        });
     }
-})
+});
